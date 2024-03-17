3b346a5efa5eb5d532a06057513a7984d1d7991b
plouneda <126429313+plouneda@users.noreply.github.com>
2024-03-17 09:21:12 -0400

    Delete main.js
    Signed-off-by: plouneda <126429313+plouneda@users.noreply.github.com>

--- main.js
+++
@@ -1,9 +0,0 @@
-function showSignup() {
-  document.getElementById('login-container').style.display = 'none';
-  document.getElementById('signup-container').style.display = 'block';
-}
-
-function showLogin() {
-  document.getElementById('signup-container').style.display = 'none';
-  document.getElementById('login-container').style.display = 'block';
-}
